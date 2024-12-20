3bd52b92e5fbd2f7f510093ddda88518e034b0a9
Phillip Vidinski <pvidinski@gmail.com>
2024-12-19 17:41:59 -0800

    started putting in the actual lagitudinal equations of motion for an aircraft

--- Dynamics/longitudinalAircraft.py
+++ Dynamics/longitudinalAircraft.py
@@ -1,4 +1,35 @@
-def solveSys(y, t, params):
-    k1, k2 = params
+import numpy as np
+
+def solveSys(y, t, controlVar):
+    k1, k2 = controlVar
+    M = 0.0; 
+    lift = 1.0 
+    drag = 2.0
+    mass = 10.0
+    g = 9.81
+    inertiay = 10.0
+
+    # states = u, w, x, z, q, theta
+    u = y[0] 
+    w = y[1]
+    x = 0 #y[2]
+    z = 0 #y[3]
+    q = 0 #y[4]
+    theta = 0  #y[5]
+
+    # axial velocity
+    u = lift/mass - g*np.sin(theta) - q*w
+    # vertical velocity
+    w = drag/mass - g*np.cos(theta) +  q*u
+
+    # Range 
+    rd = u*np.cos(theta) + w*np.sin(theta)
+
+    # Altitude
+    zd = -u*np.sin(theta) + w*np.cos(theta)
+
+    # pitch rate
+    qd = M/inertiay
+
     dydt = [-k1 * y[0] + k2 * y[1], k1 * y[0] - k2 * y[1]]
     return dydt
